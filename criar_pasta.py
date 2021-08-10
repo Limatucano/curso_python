6ec20edb71835b3f6875d3c848961d8b7d038b55
Matheus Lima <matheusmt31@gmail.com>
2021-08-10 16:26:21 -0300

    refactor de condição para zfill

--- criar_pasta.py
+++ criar_pasta.py
@@ -2,4 +2,4 @@ import os
 l = int(input('digite a quantidade de pastas que deseja:'))
 
 for x in range(1, l + 1):
-    os.mkdir(f'./secao0{x}') if x < 10 else os.mkdir(f'./secao{x}'))
+    os.mkdir(f'./secao{str(x).zfill(2)}')
