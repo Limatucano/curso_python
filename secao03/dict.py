f5798879cffd24b8c2cff6dfa0213f713b814d59
Matheus Lima <matheusmt31@gmail.com>
2021-11-11 19:16:57 -0300

    dict

---
+++ secao03/dict.py
@@ -0,0 +1,9 @@
+product = {
+        "name": "plant a",
+        "price": 20.0,
+        "description": "its a beautiful plant, the scientific name is paubrasilience",
+        "coupom" : "PLANTSTODAY"
+    }
+
+for index, value in product.items():
+    print(index, value)
